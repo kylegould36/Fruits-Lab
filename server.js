38868286691f1b17d83a04f3edca9920428d7c58
Kyle <gouldielox33@gmail.com>
2023-09-19 10:31:53 -0400

    updated

--- server.js
+++ server.js
@@ -5,16 +5,48 @@ const jsxEngine = require("jsx-view-engine");
 const fruits = require("./models/fruits.js"); //NOTE: it must start with ./ if it's just a file, not an NPM package
 const vegetables = require("./models/vegetables.js"); // Import the vegetables array
 
+//Adding our views templates
 app.set("view engine", "jsx");
 app.engine("jsx", jsxEngine());
 
+//near the top, around other app.use() calls
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  console.log("I run for all routes");
+  next();
+});
+
+//routes INDUCES
+//Index route - ALL the fruits
 app.get("/fruits/", (req, res) => {
-  // res.send(fruits);
   res.render("fruits/Index", { fruits: fruits });
 });
 
+//New - get the form to add a new fruit
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/New");
+});
+
+//Delete
+//Update
+//Create
+app.post("/fruits", (req, res) => {
+  if (req.body.readyToEat === "on") {
+    //if checked, req.body.readyToEat is set to 'on'
+    req.body.readyToEat = true; //do some data correction
+  } else {
+    //if not checked, req.body.readyToEat is undefined
+    req.body.readyToEat = false; //do some data correction
+  }
+  fruits.push(req.body);
+  res.redirect("/fruits"); //send the user back to /fruits
+});
+
+//Edit
+
+//Show route - one particular fruit by ID
 app.get("/fruits/:indexOfFruitsArray", (req, res) => {
-  // res.send(fruits[req.params.indexOfFruitsArray]);
   res.render("fruits/Show", {
     //second param must be an object
     fruit: fruits[req.params.indexOfFruitsArray], //there will be a variable available inside the ejs file called fruit, its value is fruits[req.params.indexOfFruitsArray]
